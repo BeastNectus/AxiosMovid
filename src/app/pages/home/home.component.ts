7bb14dab52f422732a20fd9c3bcc90de31238c0a
John Mamanao <kingfrost1143@gmail.com>
2024-04-03 09:40:28 +0800

    added responsiveness to the swiper slides

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MoviePopular } from '../../services/movie-popular.service';
 import { TvPopularService } from '../../services/tv-popular.service';
@@ -17,6 +17,8 @@ export class HomeComponent implements OnInit {
   searchQuery: string = '';
   searchResults: any[] = [];
   showSearchResults: boolean = false;
+  slidesPerView: number = 5;
+  screenWidth!: number;
 
   constructor(private movieService: MoviePopular, private tvService: TvPopularService, private router: Router, private multiSearchService: MultiSearchService) {}
 
@@ -24,6 +26,18 @@ export class HomeComponent implements OnInit {
     this.fetchPopularMovies(this.currentMoviePage);
     this.fetchPopularTvShows(this.currentTvPage);
   }
+
+  @HostListener('window:resize')
+    getScreenWidth() {
+        this.screenWidth = window.innerWidth
+        if (this.screenWidth >= 320 && this.screenWidth <= 480) {
+            this.slidesPerView = 1
+        } else if (this.screenWidth >= 480 && this.screenWidth <= 992) {
+            this.slidesPerView = 3
+        } else if (this.screenWidth >= 992 && this.screenWidth <= 1200) {
+            this.slidesPerView = 5
+        }
+    }
 
   fetchPopularMovies(page: number): void {
     this.movieService.getPopularMovies(page).subscribe(
